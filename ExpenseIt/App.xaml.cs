4906ea0160c92af7b53bec9fdab02a90e989f5d1
GitHubNoob <1020321552@qq.com>
2020-07-26 22:10:28 +0800

    NavigationWindow

--- ExpenseIt/App.xaml.cs
+++ ExpenseIt/App.xaml.cs
@@ -1,11 +1,9 @@
-﻿using System.Windows;
-
-namespace ExpenseIt
+﻿namespace ExpenseIt
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
     }
 }
--- ExpenseIt/MainWindow.xaml.cs
+++ ExpenseIt/MainWindow.xaml.cs
@@ -1,11 +1,12 @@
 ﻿using System.Windows;
+using System.Windows.Navigation;
 
 namespace ExpenseIt
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
