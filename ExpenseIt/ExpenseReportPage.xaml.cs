dc26fdb04084cd1783aaac9c04ba9d4add898cea
GitHubNoob <1020321552@qq.com>
2020-07-26 22:24:13 +0800

    添加page

---
+++ ExpenseIt/ExpenseReportPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace ExpenseIt
+{
+    public partial class ExpenseReportPage : Page
+    {
+        public ExpenseReportPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
